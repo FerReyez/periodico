4f5bdc09fb8f15d23f46ca5d2c8436bde4a70bbf
Lucifer <fer.reyes151719@gmail.com>
2021-02-10 17:39:50 -0600

    xD

--- application/views/template_web/login/View_template.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-$this->load->view('template_web/login/View_header');
-$this->load->view('template_web/login/View_content');
-$this->load->view('template_web/login/View_footer');
