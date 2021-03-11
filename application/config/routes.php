18d2664f83e3dd462731e30d3e1af05b32d054cd
Gilberto98 <gilberto.ponce14@gmail.com>
2021-03-11 17:44:14 -0600

    c

--- application/config/routes.php
+++ application/config/routes.php
@@ -23,6 +23,7 @@ $route['tema']                      = 'Controller_tema/vista_tema';
 $route['noticia_admin']                      = 'periodico/Controller_noticia/vista_noticia';
 $route['edicion_admin']                      = 'periodico/Controller_edicion/vista_edicion';
 $route['categoria_admin']                    = 'periodico/Controller_categoria/vista_categoria';
+$route['perfiles']                           = 'periodico/Controller_perfiles/vista_perfiles';
 
 /***********************************Web**********************************************/
 $route['inicio'] = 'Controller_web/index_web';
---
+++ application/models/Model_perfiles.php
@@ -0,0 +1,92 @@
+<?php
+
+class Model_perfiles extends CI_Model {
+
+    public $column = array(
+        'idperfiles',
+        'nombre',
+        'info',        
+        'url_foto',
+        'cargo',
+        'fecha_crea',        
+        'estado',
+    );
+
+    public $order = array('idperfiles' => 'desc');
+
+    private function _get_perfiles($term = '')
+    {
+        $column = array(
+            'id_edicion',
+            'fecha_publicacion',
+            'num_edicion',
+            'estado',
+        );
+
+        $this->db->select('*');
+        $this->db->from('perfiles');
+        $this->db->group_start();
+        $this->db->like('nombre', $term);
+        $this->db->or_like('estado', $term);
+        $this->db->group_end();
+        if (isset($_REQUEST['order'])) // here order processing
+        {
+            $this->db->order_by($column[$_REQUEST['order']['0']['column']], $_REQUEST['order']['0']['dir']);
+        } else if (isset($this->order)) {
+            $order = $this->order;
+            $this->db->order_by(key($order), $order[key($order)]);
+        }
+
+    }
+
+    public function lista_perfiles()
+    {
+        $term = $_REQUEST['search']['value'];
+        $this->_get_perfiles($term);
+        if ($_REQUEST['length'] != -1) {
+            $this->db->limit($_REQUEST['length'], $_REQUEST['start']);
+        }
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function count_filtered()
+    {
+        $term = $_REQUEST['search']['value'];
+        $this->_get_perfiles($term);
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    public function count_all()
+    {   
+        $this->db->select('*');
+        $this->db->from('edicion');
+        return $this->db->count_all_results();
+    }
+
+    public function crear_perfil($table, $data) {
+        $result = $this->db->insert($table, $data);
+        return $result;
+    }
+
+    public function linea_actualizar($table, $editBtnId) {
+        $this->db->where('idperfiles', $editBtnId);
+        $result = $this->db->get($table);
+        return $result->result();
+    }
+
+    public function actualizar_perfil($table, $data, $updateId) {
+        $this->db->where('idperfiles', $updateId);
+        $result = $this->db->update($table, $data);
+        return $result;
+    }
+
+    public function eliminar_edicion($table, $delteBtnId) {
+        $this->db->where('idperfiles', $delteBtnId);
+        $result = $this->db->delete($table);
+        return $result;
+    }
+
+}
