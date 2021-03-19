a306eaa489f452ba8ab5ea6c1ac85f441381d9e1
Lucifer <fer.reyes151719@gmail.com>
2021-03-18 19:15:24 -0600

    Nota Comentarios

--- application/views/periodico/View_perfiles.php
+++ application/views/periodico/View_perfiles.php
@@ -183,20 +183,7 @@
                     <div class="modal-body users-cont">
                         <div class="row clearfix">
                             <div class="body">
-                                <textarea id="ckeditor">
-                                    <h2>WYSIWYG Editor</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ullamcorper sapien non nisl facilisis bibendum in quis tellus. Duis in urna bibendum turpis pretium fringilla. Aenean neque velit, porta eget mattis ac, imperdiet quis nisi. Donec non dui et tortor vulputate luctus. Praesent consequat rhoncus velit, ut molestie arcu venenatis sodales.</p>
-                                    <h3>Lacinia</h3>
-                                    <ul>
-                                        <li>Suspendisse tincidunt urna ut velit ullamcorper fermentum.</li>
-                                        <li>Nullam mattis sodales lacus, in gravida sem auctor at.</li>
-                                        <li>Praesent non lacinia mi.</li>
-                                        <li>Mauris a ante neque.</li>
-                                        <li>Aenean ut magna lobortis nunc feugiat sagittis.</li>
-                                    </ul>
-                                    <h3>Pellentesque adipiscing</h3>
-                                    <p>Maecenas quis ante ante. Nunc adipiscing rhoncus rutrum. Pellentesque adipiscing urna mi, ut tempus lacus ultrices ac. Pellentesque sodales, libero et mollis interdum, dui odio vestibulum dolor, eu pellentesque nisl nibh quis nunc. Sed porttitor leo adipiscing venenatis vehicula. Aenean quis viverra enim. Praesent porttitor ut ipsum id ornare.</p>
-                                </textarea>
+                                <textarea name="comentario" id="comentario"></textarea>
                             </div>
                         </div>
                     </div>
@@ -213,8 +200,12 @@
 
 
 <script src="<?php echo base_url(); ?>assets/select2/js/select2.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/ckeditor/ckeditor.js"></script>
-<script src="<?php echo base_url(); ?>assets/plugins/tinymce/tinymce.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/pages/forms/editors.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/main_js/perfiles.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/main_js/comentario.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/ckeditor/ckeditor.js"></script>
+<script>
+    CKEDITOR.replace('comentario', {
+        customConfig: '<?php echo base_url(); ?>assets/plugins/ckeditor/ckeditor_nota.js'
+    });
+    CKEDITOR.config.height = 500;
+</script>
