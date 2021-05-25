dc6c220ad5aec6d3d11604834008923dfe6ff6d8
Lucifer <fer.reyes151719@gmail.com>
2021-05-24 19:11:34 -0600

    Ediciones

--- application/controllers/periodico/Controller_edicionPrincipal.php
+++ application/controllers/periodico/Controller_edicionPrincipal.php
@@ -9,13 +9,19 @@ class Controller_edicionPrincipal extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Usuarios');
+        $this->load->model('Model_web');
     }
 
-    public function cargar_plantilla($vista, $data)
-    {
+    public function cargar_plantilla($vista, $data){
+        $menus = $this->Model_web->listar_menu();
+        $opciones = $this->Model_web->listar_opcion();
+        $redes = $this->Model_web->listar_redes();
+        
+        $data['menus'] = $menus;
+        $data['opciones'] = $opciones;
+        $data['redes'] = $redes;
         $data['main_content'] = $vista;
-        $this->load->view('template/template', $data);
+        $this->load->view('template_web/web/View_template', $data);
     }
 
     public function vista_edicionPrincipal()
